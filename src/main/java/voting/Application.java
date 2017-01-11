e320329a35e39d1d7536eb111de907c9ffad5180
Domas Karklys <domas.karklys@gmail.com>
2017-01-11 15:03:50 +0200

    Initial commit

---
+++ src/main/java/voting/Application.java
@@ -0,0 +1,15 @@
+package voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by domas on 1/10/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

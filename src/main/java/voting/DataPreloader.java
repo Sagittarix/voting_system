6d559adcf99595d574e332ac8d4fd86ec27d064b
Domas Karklys <domas.karklys@gmail.com>
2017-01-15 21:04:44 +0200

    Added random person ID generator and added valid personIds to candidates

--- src/main/java/voting/DataPreloader.java
+++ src/main/java/voting/DataPreloader.java
@@ -9,10 +9,8 @@ import voting.repository.CountyRepRepository;
 import voting.repository.DistrictRepository;
 import voting.repository.PartyRepository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 /**
  * Created by domas on 1/11/17.
@@ -20,6 +18,7 @@ import java.util.List;
 @Component
 public class DataPreloader implements CommandLineRunner {
 
+    private static Random rand = new Random(0);
     private final CountyRepRepository countyRepRepository;
     private final DistrictRepository districtRepository;
     private final PartyRepository partyRepository;
@@ -41,10 +40,6 @@ public class DataPreloader implements CommandLineRunner {
         District district2 = new District("Kaunas");
         District district3 = new District("Klaipėda");
 
-        districtRepository.save(district1);
-        districtRepository.save(district2);
-        districtRepository.save(district3);
-
         County county1 = new County("Senamiestis", 3000L);
         County county2 = new County("Naujamiestis", 3000L);
         County county3 = new County("Pašilaičiai", 3000L);
@@ -57,6 +52,9 @@ public class DataPreloader implements CommandLineRunner {
         countyList1.forEach(c -> district1.addCounty(c));
         countyList2.forEach(c -> district2.addCounty(c));
 
+        districtRepository.save(district1);
+        districtRepository.save(district2);
+        districtRepository.save(district3);
 
         Party party1 = new Party("Demokratai", "DEM");
         Party party2 = new Party("Tvarka ir tesingumas", "TT");
@@ -64,15 +62,15 @@ public class DataPreloader implements CommandLineRunner {
 
         partyRepository.save(Arrays.asList(party1, party2, party3));
 
-        Candidate cand1 = new Candidate("111", "Jonas", "Joinaitis");
-        Candidate cand2 = new Candidate("222", "Petras", "Petraitis");
-        Candidate cand3 = new Candidate("333", "Jonas", "Kubilius");
-        Candidate cand4 = new Candidate("444", "Jonas", "Adamkus");
-        Candidate cand5 = new Candidate("555", "Jonas", "Grybas");
-        Candidate cand6 = new Candidate("666", "Jonas", "Alekna");
-        Candidate cand7 = new Candidate("777", "Jonas", "Juršėnas");
-        Candidate cand8 = new Candidate("888", "Jonas", "Uspaskichas");
-        Candidate cand9 = new Candidate("999", "Jonas", "Aparatas");
+        Candidate cand1 = new Candidate("11122233301", "Jonas", "Joinaitis");
+        Candidate cand2 = new Candidate("11122233302", "Petras", "Petraitis");
+        Candidate cand3 = new Candidate("11122233303", "Jonas", "Kubilius");
+        Candidate cand4 = new Candidate("11122233304", "Jonas", "Adamkus");
+        Candidate cand5 = new Candidate("11122233305", "Jonas", "Grybas");
+        Candidate cand6 = new Candidate("11122233306", "Jonas", "Alekna");
+        Candidate cand7 = new Candidate("11122233307", "Jonas", "Juršėnas");
+        Candidate cand8 = new Candidate("11122233308", "Jonas", "Uspaskichas");
+        Candidate cand9 = new Candidate("11122233309", "Jonas", "Aparatas");
 
         List<Candidate> candidateList1 = new ArrayList<Candidate>(Arrays.asList(cand1, cand2, cand3, cand4));
         List<Candidate> candidateList2 = new ArrayList<Candidate>(Arrays.asList(cand5, cand6, cand7));
@@ -94,10 +92,12 @@ public class DataPreloader implements CommandLineRunner {
             district2.addCandidate(c);
             candidateRepository.save(c);
         });
-
-
-
     }
 
-
+    public static String generateRandomPersonId() {
+        StringBuilder id = new StringBuilder();
+        rand.ints(11, 0, 10).forEach(digit -> id.append(digit));
+        System.out.println(id.toString());
+        return id.toString();
+    }
 }

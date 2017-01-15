68181f594d6f4d2e1fafa9bc8905dc24df83ca2e
Domas Karklys <domas.karklys@gmail.com>
2017-01-15 16:14:48 +0200

    Added party controller

---
+++ src/main/java/voting/controller/PartyController.java
@@ -0,0 +1,44 @@
+package voting.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import voting.model.Party;
+import voting.model.PartyData;
+import voting.service.PartyService;
+
+import java.util.List;
+
+/**
+ * Created by domas on 1/15/17.
+ */
+@RestController
+@RequestMapping(path="/api/party")
+public class PartyController {
+
+    private final PartyService partyService;
+
+    @Autowired
+    public PartyController(PartyService partyService) {
+        this.partyService = partyService;
+    }
+
+    @GetMapping
+    public List<Party> getParties() {
+        return partyService.getParties();
+    }
+
+    @GetMapping("/{id}")
+    public Party getParty(@PathVariable Long id) {
+        return partyService.getParty(id);
+    }
+
+    @PostMapping
+    public Party addNewParty(@RequestBody PartyData partyData) {
+        return partyService.addNewParty(partyData);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteParty(@PathVariable Long id) {
+        partyService.deleteParty(id);
+    }
+}

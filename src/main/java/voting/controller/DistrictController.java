7c028b58d188307c956b30fb4841dbcc0bdd092a
Domas Karklys <domas.karklys@gmail.com>
2017-01-28 17:20:48 +0200

    Refactor - changed to global exception handling

--- src/main/java/voting/controller/DistrictController.java
+++ src/main/java/voting/controller/DistrictController.java
@@ -2,18 +2,10 @@ package voting.controller;
 
 import com.opencsv.exceptions.CsvException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.multipart.MultipartFile;
 import voting.dto.DistrictData;
 import voting.dto.DistrictRepresentation;
-import voting.exception.ErrorResponse;
-import voting.exception.NotFoundException;
-import voting.exception.StorageException;
-import voting.exception.StorageFileNotFoundException;
 import voting.service.DistrictService;
 
 import javax.validation.Valid;
@@ -68,27 +60,4 @@ public class DistrictController {
         return new DistrictRepresentation(districtService.setCandidateList(id, file));
     }
     
-
-    @ExceptionHandler({CsvException.class, StorageException.class, IOException.class})
-    protected ResponseEntity<Object> handleSomeException(Exception ex, WebRequest request) {
-        HttpStatus status = null;
-        if (ex instanceof CsvException) {
-            status = HttpStatus.BAD_REQUEST;
-        } else if (ex instanceof StorageFileNotFoundException) {
-            status = HttpStatus.NOT_FOUND;
-        } else if (ex instanceof StorageException) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-        } else if (ex instanceof IOException) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-        } else if (ex instanceof NotFoundException) {
-            status = HttpStatus.NOT_FOUND;
-        } else if (ex instanceof IllegalArgumentException) {
-            status = HttpStatus.CONFLICT;
-        }
-        ErrorResponse body = new ErrorResponse();
-        body.setErrorCode(status.value());
-        body.setMessage(ex.getMessage());
-        return new ResponseEntity(body, new HttpHeaders(), status);
-    }
-
 }

3f887b4ed811a1eac0fd9b8c72d60d6120985eaa
Domas Karklys <domas.karklys@gmail.com>
2017-03-06 21:39:42 +0200

    Updated frontend

--- src/main/app/js/admin_results_view/containers/AdminResultsViewContainer.js
+++ src/main/app/js/admin_results_view/containers/AdminResultsViewContainer.js
@@ -6,21 +6,30 @@ var spring = require('../../config/SpringConfig');
 
 var AdminResultsViewContainer = React.createClass({
 	getInitialState: function() {
-		return ({ districts: [],
+		return ({ optionsMap: new Map(),
 				  counties: [],
-				  activeDistrictId: 0,
-				  activeCountyId: 0 });
+				  activeDistrict: 0,
+				  activeCounty: 0 });
 	},
 	componentDidMount: function() {
 		var _this = this;
 
-		axios.get(spring.localHost.concat('/api/district'))
+		axios.get(spring.localHost.concat('/api/county'))
 			.then(function(response) {
-				var counties = [];
-				response.data.forEach(d => {
-					d.counties.forEach(c => counties.push(c));
+				
+				var counties = response.data;
+				var optionsMap = new Map();
+
+				counties.forEach(county => {
+					var district = county.district.name
+					if (optionsMap.get(district)) {
+						optionsMap.get(district).push(county)
+					} else {
+						optionsMap.set(district, [county])
+					}
 				});
-				_this.setState({ districts: response.data,
+
+				_this.setState({ optionsMap: optionsMap,
 								 counties: counties });
 			})
 			.catch(function(err) {
@@ -29,112 +38,92 @@ var AdminResultsViewContainer = React.createClass({
 
 	},
 	setActiveDistrict: function(e) {
-		this.setState({ activeDistrictId: e.target.value });
+		this.setState({ activeDistrict: e.target.value, activeCounty: 0 });
 	},
 	setActiveCounty: function(e) {
-		this.setState({ activeCountyId: e.target.value });
+		this.setState({ activeCounty: e.target.value });
+	},
+	filterByCounty(county) {
+		return county.name === this.state.activeCounty
+	},
+	filterByDistrict(county) {
+		return county.district.name === this.state.activeDistrict
 	},
 	prepareCounties() {
 		var counties = this.state.counties;
 		var preparedCounties = [];
 
-		if (this.state.activeDistrictId != 0) {
-			if (this.state.activeCountyId != 0) {
-				counties.forEach((c, idx) => {
-					if (this.state.activeCountyId == c.id) {
-						preparedCounties.push(
-							<CountyDisplayContainer
-								key={idx}
-								index={idx}
-								county={c}
-							/>
-						);
-					}
-				});
-			} else {
-				counties.forEach((c, idx) => {
-					if (this.state.activeDistrictId == c.districtId) {
-						preparedCounties.push(
-							<CountyDisplayContainer
-								key={idx}
-								index={idx}
-								county={c}
-							/>
-						);
-					}
-				});
-			}
-		} else {
-			counties.forEach((c, idx) => {
-				preparedCounties.push(
-					<CountyDisplayContainer
-						key={idx}
-						index={idx}
-						county={c}
-					/>
-				);
-			});
+		if (this.state.activeCounty != 0) {
+			counties = counties.filter(this.filterByDistrict).filter(this.filterByCounty)
+		} else if (this.state.activeDistrict != 0) {
+			counties = counties.filter(this.filterByDistrict)
 		}
+
+		var preparedCounties = counties.map((c, idx) => {
+			return (
+				<CountyDisplayContainer
+					key={c.district.id + "." + c.id}
+					index={idx}
+					unit={c}
+				/>
+			)
+		})
+
 		return preparedCounties;
 	},
 	districtsSelect: function() {
-		var districts = this.state.districts;
-		var preparedDistricts = [];
+		var districtOptions = [];
 
-		districts.forEach((d, idx) => {
-			preparedDistricts.push(
+		this.state.optionsMap.forEach((v, k) => {
+			districtOptions.push(
+				<option 
+					value={k} 
+					key={k + "-" + v.name}
+				> 
+					{k} 
+				</option>
+			);			
+		})
+
+		return (
+			<select value={this.state.activeDistrict} onChange={this.setActiveDistrict}>
 				<option
-					value={d.id}
-					key={idx}
+					value={0}
+					key={districtOptions.length}
 				>
-					{d.name}
+					Visos apygardos
+				</option>
+				{districtOptions}
+			</select>
+		);
+	},
+	countiesSelect: function() {
+		var countyOptions = [];
+		var dict = this.state.optionsMap
+		var activeDistrict = this.state.activeDistrict
+
+		if (activeDistrict == 0) return undefined;
+
+		dict.get(activeDistrict).forEach(c => {
+			countyOptions.push(
+				<option
+					value={c.name}
+					key={activeDistrict + '-' + c.name}
+				>
+					{c.name}
 				</option>
 			);
 		});
 
 		return (
-			<select value={this.state.activeDistrictId} onChange={this.setActiveDistrict}>
+			<select value={this.state.activeCounty} onChange={this.setActiveCounty}>
 				<option
 					value={0}
-					key={districts.length}
-				>
-					Visos apygardos
-				</option>
-				{preparedDistricts}
-			</select>
-		);
-	},
-	countiesSelect: function() {
-		var counties = this.state.counties;
-		var filteredCounties = [];
-
-		if (this.state.activeDistrictId == 0) return undefined;
-
-
-		else {
-			counties.forEach((c, idx) => {
-				if (c.districtId == this.state.activeDistrictId) {
-					filteredCounties.push(
-						<option
-							value={c.id}
-							key={idx}
-						>
-							{c.name}
-						</option>
-					);
-				}
-			});
-		}
-
-		return (
-			<select value={this.state.activeCountyId} onChange={this.setActiveCounty}>
-				<option
-					value={0}
-					key={counties.length}
+					key={countyOptions.length}
 				>
 					Visos apylinkės
 				</option>
-				{filteredCounties}
+				{countyOptions}
 			</select>
 		);
 	},

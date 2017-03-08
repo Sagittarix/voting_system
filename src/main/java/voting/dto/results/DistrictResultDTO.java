dc0c429c85f16b9efdcd6b3232cce0ad9f2377fb
Domas Karklys <domas.karklys@gmail.com>
2017-03-08 10:46:23 +0200

    Added conirmed/total counties to District result dto

--- src/main/java/voting/dto/results/DistrictResultDTO.java
+++ src/main/java/voting/dto/results/DistrictResultDTO.java
@@ -1,6 +1,7 @@
 package voting.dto.results;
 
 import voting.dto.district.DistrictShortDTO;
+import voting.model.District;
 import voting.results.model.result.DistrictResult;
 import voting.results.model.result.DistrictSMResult;
 import voting.results.model.result.ResultType;
@@ -14,13 +15,16 @@ import java.util.stream.Collectors;
 public class DistrictResultDTO extends ResultDTO {
 
     private DistrictShortDTO district;
+    private Long confirmedCountyResults;
+    private int totalCounties;
     private List<CountyResultSummaryDTO> countyResults;
 
 
     public DistrictResultDTO(DistrictResult result) {
         super(result);
-        this.district = result.getDistrict() == null ? null : new DistrictShortDTO(result.getDistrict());
-        this.setVoterCount(result.getDistrict().getVoterCount());
+        District district = result.getDistrict();
+        this.district = district == null ? null : new DistrictShortDTO(district);
+        this.setVoterCount(district.getVoterCount());
 
         ResultType type;
         if (result instanceof DistrictSMResult) {
@@ -29,13 +33,24 @@ public class DistrictResultDTO extends ResultDTO {
             type = ResultType.MULTI_MANDATE;
         }
 
-        this.countyResults = result.getDistrict().getCounties().stream()
+        this.countyResults = district.getCounties().stream()
                 .map(c -> new CountyResultSummaryDTO(c, type))
                 .collect(Collectors.toList());
+
+        this.confirmedCountyResults = countyResults.stream().filter(r -> r.getCreatedOn() != null).count();
+        this.totalCounties = countyResults.size();
     }
 
     public DistrictShortDTO getDistrict() {
         return district;
+    }
+
+    public Long getConfirmedCountyResults() {
+        return confirmedCountyResults;
+    }
+
+    public int getTotalCounties() {
+        return totalCounties;
     }
 
     public List<CountyResultSummaryDTO> getCountyResults() {
